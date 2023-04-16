f8f6c3b7d4d3f208c7b30af6560858eb4a489ad3
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-04-16 07:30:02 +0000

    create item

---
+++ db/migrate/20230416072525_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :items do |t|
+      t.integer :category_id
+      t.string :name
+      t.integer :sale_status, null: false, default: true
+      t.text :introduction
+      t.integer :price_with_tax
+      t.timestamps
+    end
+  end
+end

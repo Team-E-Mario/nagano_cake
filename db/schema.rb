45e86a29f117fb51e2dd7acd74df07dbf038385b
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-04-16 07:32:46 +0000

    create category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_16_072525) do
+ActiveRecord::Schema.define(version: 2023_04_16_073129) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 2023_04_16_072525) do
     t.integer "item_id"
     t.integer "customer_id"
     t.integer "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

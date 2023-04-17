934f2451b1615dbef9429b2a4a3b5073dfb49d4c
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-04-17 07:13:26 +0000

    create association

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,10 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
+  has_many :cart_items, dependent: :destroy
+  belongs_to :category
+  has_many :order_items, dependent: :destroy
+
 
   def get_image(width, height)
     unless image.attached?

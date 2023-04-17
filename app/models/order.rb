dd0dcee6e0d96c4316692dda51fa065a1b2030f3
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-04-17 07:42:27 +0000

    create enum

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,11 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items, dependent: :destroy
+
+  # 注文ステータス
+  enum status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered: 4 }
+
+  # 支払い方法
+  enum payment_method: { credit_card: 0, transfer: 1 }
+
 end

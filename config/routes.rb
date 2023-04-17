da6606869d614c18b1a2c8ce8ad2337beadbc6be
AkihiroTanaka3 <qqqwwetyu@gmail.com>
2023-04-17 04:41:02 +0000

    [controller create]

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
 
+  devise_for :admins
+  devise_for :customers
   # 管理者側
   namespace :admin do
     root to: 'homes#top'
+    resources :items, only: [:index, :new, :show, :edit, :create, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :orders_items, only: [:update]
+    resources :customers, only: [:index, :show, :edit]
   end
   # 顧客側
   scope module: :public do
@@ -14,8 +21,9 @@ Rails.application.routes.draw do
     get "/orders/confirm" => "orders#confirm", as: "confirm"
     resources :orders, only: [:new, :index, :show, :create]
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+    get '/customers/unsubscribe' => 'customers#unsubscribe', as: 'unsubscribe'
+    patch '/customers/withdraw' => 'customers#withdraw', as: 'withdraw'
+    resource :customers, only: [:show, :edit, :update]
   end
-  devise_for :admins
-  devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

a52fe1258be53bfb81d91fe6b33595ec57634ab3
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-04-18 02:58:35 +0000

    マイページ作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,8 +1,15 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    @customer.update
   end
 
   def unsubscribe
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -29,7 +29,7 @@ class Public::SessionsController < Devise::SessionsController
   # end
 
   def after_sign_in_path_for(resource)
-    customers_path
+    root_path
   end
 
   def reject_withdraw_customer

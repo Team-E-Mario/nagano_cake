5faa817871ab970a8c3e575c98c65a97e26a0c49
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-04-19 06:27:56 +0000

    途中

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,32 @@
 class Public::AddressesController < ApplicationController
+
   def index
+    @addresses = Address.all
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
+
+  def create
+    @address = Address.new(address_params)
+    @address.save
+    redirect_to 
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to
+  end
+
+  def destroy
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:address, :postcode, :name, :customer_id)
+  end
+
 end

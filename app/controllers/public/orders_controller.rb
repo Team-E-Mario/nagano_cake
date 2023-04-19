6dba9a857e71a0fff113e00dee137f2b30dcd9d5
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-04-19 10:04:33 +0000

    途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,8 +1,19 @@
 class Public::OrdersController < ApplicationController
+
   def index
   end
 
   def new
+    @order = Order.new
+    @delivery_address = current_customer.delivery_address
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.freight = 800
+    @order.save
+    redirect_to orders_thanx_path
   end
 
   def show
@@ -13,4 +24,11 @@ class Public::OrdersController < ApplicationController
 
   def confirm
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:delivery_postcode, :delivery_address, :delivery_name, :billing_amount, :payment_method, :customer_id, :status)
+  end
+
 end

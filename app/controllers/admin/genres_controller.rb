14515a0fa4e69883fb873acebf7c1fa54c95136c
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-04-19 03:07:10 +0000

    add_ジャンル作成機能

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,26 @@
 class Admin::GenresController < ApplicationController
+  
   def index
+    @categories = Category.all
+    @category = Category.new
   end
 
   def edit
   end
+  
+  def create
+    @category = Category.new(category_params)
+    @category.save
+    redirect_to admin_genres_path
+  end
+  
+  def update
+  end
+  
+  private
+  
+  def category_params
+    params.require(:category).permit(:name)
+  end
+  
 end

67f8dd90e69a8e9643ec106d9f92d8b7474f4d0d
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-04-22 15:33:08 +0000

    Update_admin_注文履歴詳細_途中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,13 @@
 class Admin::OrdersController < ApplicationController
-  
+
   def show
+    @order = Order.find(params[:id])
+    @orders = Order.all
+    @order_items = OrderItem.where(order_id: @order)
+    @product_status = @order.order_items.pluck(:product_status)
   end
-  
+
   def update
   end
-  
+
 end

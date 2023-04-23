dacbbab1e2ec335fce31b770879284e18b8830b4
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-04-23 02:56:02 +0000

    Update_admin注文履歴詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,6 +8,21 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order_items = OrderItem.where(order_id: @order)
+    @order.update(order_params)
+
+    if @order.status == "入金確認"
+      @order_items.update_all(product_status: 1)
+    end
+    redirect_to admin_order_path(@order)
+
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
   end
 
 end

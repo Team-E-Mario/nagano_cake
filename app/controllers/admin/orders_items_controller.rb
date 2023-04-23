58a8454572c7bb70d7a491aa145ea80f8fe924e8
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-04-23 09:18:02 +0000

    注文履歴詳細機能途中

--- app/controllers/admin/orders_items_controller.rb
+++ app/controllers/admin/orders_items_controller.rb
@@ -1,2 +1,24 @@
 class Admin::OrdersItemsController < ApplicationController
+  
+  def update
+    @order = Order.find(params[:id])
+    @order.item = OrderItem.find(params[:order_item][order_item.id])
+    if @order_item.update(order_item_params)
+      if @order.order_items.pluck(:product_status).include?("製作中")
+        @order.status = 2
+        @order.save
+      elsif
+        @order.order_items.pluck(:product_status).all?{|status|status == "製作完了"}
+        @order.status = 3
+        @order.save
+      end
+    end
+  end
+  
+  private
+  
+  def order_item_params
+    params.require(:order_item).permit(:product_status)
+  end
+  
 end

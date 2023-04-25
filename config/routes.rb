0dade797878797a307d85831d4e62b4f232fd37f
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-04-25 05:44:21 +0000

    修正_admin_item

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
     root to: 'homes#top'
     resources :items, only: [:index, :new, :show, :edit, :create, :update]
     resources :genres, only: [:index, :create, :edit, :update,]
-    resources :orders, only: [:index, :show, :update]
+    resources :orders, only: [:show, :update]
     resources :orders_items, only: [:update]
     resources :customers, only: [:index, :show, :edit, :update]
   end
